315d2e5fb945016c7f7c99efbb3c8426dff79daa
captainsmash1814 <parkerjunaid@gmail.com>
2018-12-17 21:00:05 +0200

    Test
    test

---
+++ Assets/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 100;
+    private float moveX;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+
+    }
+
+    void PlayerMove()
+    {
+        //Controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump")){
+            Jump();
+
+        }
+        //Animations
+        //Direction
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+
+        } else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+
+        }
+
+        //Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+
+    }
+
+    void Jump() {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+
+    }
+
+
+    void FlipPlayer() {
+
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+
+    }
+}
+
+
+
+    
+
